c31a757582be292b09097bc798b2264e090a2374
Stefan Stefanov <stefan.stefanov@jakala.com>
2025-07-02 22:15:46 +0300

    Simplify date selection UI/UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { Box, Heading, VStack, Input } from "@chakra-ui/react";
+import { Box, Heading, VStack, Button, HStack, Text } from "@chakra-ui/react";
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
+import { format } from 'date-fns';
 import DailyMenu from '@/components/DailyMenu';
 import DatePicker from '@/components/DatePicker';
 
@@ -24,22 +25,24 @@ function HomeContent() {
     <Box minH="100vh" p={8}>
       <VStack spacing={8} maxW="800px" mx="auto">
         <Heading size="xl" textAlign="center">
-          BCVT Menu for {selectedDate.toDateString()}
-        </Heading>
-        
-        <DatePicker
-          selected={selectedDate}
-          onChange={handleDateChange}
-          customInput={
-            <Input 
-              placeholder="Select a date"
-              size="lg"
-              cursor="pointer"
-              readOnly
+          BCVT Menu for{' '}
+          <HStack spacing={3} display="inline-flex" alignItems="center">
+            <Text>{format(selectedDate, 'dd.MM.yyyy')}</Text>
+            <DatePicker
+              selected={selectedDate}
+              onChange={handleDateChange}
+              customInput={
+                <Button
+                  variant="outline"
+                  size="sm"
+                >
+                  📅
+                </Button>
+              }
+              dateFormat="dd.MM.yyyy"
             />
-          }
-          dateFormat="dd.MM.yyyy"
-        />
+          </HStack>
+        </Heading>
         
         <DailyMenu date={selectedDate} />
       </VStack>
