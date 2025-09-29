953cc90cc3b220a0b5762614be19482c8abc3252
Stefan Stefanov <stefan.stefanov@jakala.com>
2025-09-29 20:45:51 +0300

    Fix headings missalignment

--- src/app/[locale]/(menu)/layout.tsx
+++ src/app/[locale]/(menu)/layout.tsx
@@ -5,7 +5,7 @@ import { Fragment, type FC, type PropsWithChildren } from 'react';
 const MenuLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
-      <Heading size="lg" textAlign="center">
+      <Heading size="lg">
         <HStack as="span">
           <Text color="gray.500" as="span">BCVT Menu{' '}</Text>
           <RefDate />
